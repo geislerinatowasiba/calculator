184d6c6ed468eee476da126a271a7ca2febeaab5
geislerinatowansiba <geislerinatowansiba@students.amikom.ac.id>
2023-03-16 14:46:08 +0700

    menambah file

---
+++ callculator.cs
@@ -0,0 +1,24 @@
+class calculator
+
+   static void main(string[] args)
+   
+      int a = 10;
+	  int b = 6;
+	  
+	  Console.WriteLiner("hasil penambahan: {0} + {1} = {2}", a, b, penambahan(a, b));
+	  Console.WriteLiner("hasil penambahan: {0} + {1} = {2}", a, b, pengurangan(a, b));
+	  
+	  Console.WriteLiner("\nTekan sembarang Key untuk keluar");
+	  Console.WriteLiner.ReadKey();
+	  }
+	  static int penambahan(int a, int b)
+	  }
+	    return a + b;
+	  }
+	  static int pengurangan(int a, int b)
+	  {
+	    return a - b;
+		
+}
+	  
+	  
